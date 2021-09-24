774d43bd95e324127c95288a0bc5c9f9f8bd6e61
metalentco <metalentco777@gmail.com>
2021-09-24 05:11:28 -0700

    upload

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# opensea-bid-bot
-Proxy supported, multi-wallet pensea bid bot. Automatically bid on collections WITHOUT DELAYS.
+# opensea bid bot
+Opensource opensea bid bot.
+Proxies are not required (but recommended if speed is what you are looking for).
+
+### main features
+
+- multi rpc support
+- multi wallet support
+- proxy option (avoid opensea ratelimit)
+
+
+### configuration
+
+OS bid bot requires [Node.js](https://nodejs.org/) to run.
+You will also need an [infura.io](https://infura.io/dashboard/ethereum) account for the RPC provider.
+
+Note: The bot will start a thread for every mnemonic/privatekey. This means that you can have more of the same mnemonic/privatekey in your config so it starts more threads, resulting in a faster bot.
+
+Edit the `config.json` file to your needs.
+
+`rpc_urls` -> an array of rpc providers (one or more)
+`mnemonics` -> an array of mnemonics
+`privateKeys` -> an array of private keys
+`bid_price` -> amount of `WETH` you want to bid on the assets
+`expiration` -> amount of hours in which you want your offer/bid to expire
+`timeout` -> timeout in `ms` per bid
+`proxied` -> boolean `false`/`true` . if `true`, will read proxies from `proxies.txt`
+`targets` -> array of targets with the `address` and `supply` of the collection. the `address` can be found in the url when visiting an asset from this collection; `https://opensea.io/assets/<COLLECTION_ADDRESS>/<token_id (not relevant)>`. the `supply` is the amount of tokens in existence.
+
+##### Always have either one private key or mnemonic !!!
+
+### first example `config.json`
+```json
+{
+    "rpc_urls": [
+        "https://mainnet.infura.io/v3/projectid"
+    ],
+    "mnemonics":[
+        "some random twelve word mnemonic here ..."
+    ],
+    "privateKeys":[],
+    "bid_price": 0.2,
+    "expiration":3,
+    "timeout":100,
+    "proxied":false,
+    "targets":[
+        {
+            "address":"0xd13be7ae034a8aafc786fb077eaf1b0892079315",
+            "supply":8888
+        },
+        {
+            "address":"0xdeadbeef00000000000000000000000000000000",
+            "supply":10000
+        }
+    ]
+}
+```
+
+### second example `config.json`
+```json
+{
+    "rpc_urls": [
+        "https://mainnet.infura.io/v3/projectid",
+        "https://mainnet.infura.io/v3/anotherone"
+    ],
+    "mnemonics":[
+        "some random twelve word cool mnemonic here ..."
+    ],
+    "privateKeys":[
+        "4fa5084bfbd7c6400ef9633dea1d1b329c12dadeb0aac74d4d62a5e1a43a83f3",
+        "4c329d7abf1f8c05efd6db8ffd51a601c159d75a81805be10a361cc13d7f2a9c"
+    ],
+    "bid_price": 0.08,
+    "expiration":24,
+    "timeout":1500,
+    "proxied":true,
+    "targets":[
+        {
+            "address":"0xd13be7ae034a8aafc786fb077eaf1b0892079315",
+            "supply":8888
+        }
+    ]
+}
+```
+
+
+
+### installation
+```sh
+git clone https://github.com/opensea-bots/opensea-bid-bot.git
+cd opensea-bid-bot
+npm i
+```
+
+### running the bot
+```sh
+node bot.js
+```
+
+### License
+
+MIT
+
---
+++ bot.js
@@ -0,0 +1,120 @@
+const OpenSea = require('opensea-js');
+const fs = require('fs');
+const HDWalletProvider = require("@devcrypto/hdwallet-provider");
+const config = require('./config.json');
+const HttpsProxyAgent = require('https-proxy-agent');
+const network = OpenSea.Network.Main;
+const DEBUG_LEVEL = 1;
+
+function debug(level, message)
+{
+    var time = new Date();
+    var f_time = time.getHours().toString().padStart(2,'0') + ":" + time.getMinutes().toString().padStart(2,'0') + ":" + time.getSeconds().toString().padStart(2,'0');
+    if (DEBUG_LEVEL >= level) { console.log(`[${f_time}] ${message}`); }
+}
+
+var proxies = [];
+if (config.proxied)
+{
+    const data = fs.readFileSync('proxies.txt', 'utf8');
+    data.split('\n').forEach(line => {
+        if (line.length > 3){ proxies.push(`http://${line}`); }
+    });
+    debug(2, `loaded ${proxies.length} proxies`)
+}
+
+const fetch = global.fetch;
+global.fetch = function() {
+    var url = arguments[0];
+    var options = arguments[1];
+
+    if (config.proxied) {
+        var proxy = proxies.shift();
+        proxies.push(proxy);
+        arguments[1].agent = new HttpsProxyAgent(proxy);
+        console.log(proxy)
+    }
+
+    debug(3, `opensea-sdk : ${url}`);
+
+    return new Promise((resolve, reject) => {
+        fetch.apply(this, arguments)
+        .then((response) => {
+            resolve(response);
+        })
+        .catch((error) => {
+            reject(error);
+        })
+    });
+}
+
+
+var providers = [];
+var assets = [];
+
+var rpc_index = 0;
+config.mnemonics.forEach(mnemonic => {
+    var provider = new HDWalletProvider({
+        mnemonic: {
+            phrase: mnemonic
+        },
+        providerOrUrl: config.rpc_urls[rpc_index]
+    });
+    var seaport = new OpenSea.OpenSeaPort(provider, {networkName: network});
+    providers.push({"provider":provider,"seaport":seaport});
+    seaport.addListener(OpenSea.EventType.CreateOrder, ({ order, accountAddress }) => {
+        debug(1, `order created ${accountAddress} : ${order.hash} (asset /${order.metadata.asset.id})`);
+    });
+    rpc_index++;
+    if(rpc_index == config.mnemonics.length) {rpc_index = 0;};
+})
+config.privateKeys.forEach(privateKey => {
+    var provider = new HDWalletProvider({
+        privateKeys: [privateKey],
+        providerOrUrl: config.rpc_urls[rpc_index]
+    });
+    var seaport = new OpenSea.OpenSeaPort(provider, {networkName: network});
+    providers.push({"provider":provider,"seaport":seaport});
+    seaport.addListener(OpenSea.EventType.CreateOrder, ({ order, accountAddress }) => {
+        debug(1, `order created ${accountAddress} : ${order.hash} (asset /${order.metadata.asset.id})`);
+    });
+    rpc_index++;
+    if(rpc_index == config.mnemonics.length) {rpc_index = 0;};
+})
+
+
+async function providerThread(_provider)
+{
+    var provider = _provider.provider;
+    var seaport = _provider.seaport;
+    var _asset = assets.shift();
+
+    if (_asset != undefined) {
+        const offer = await seaport.createBuyOrder({
+            asset: {
+                tokenId:_asset.tokenId,
+                tokenAddress:_asset.address
+            },
+            accountAddress:provider.getAddress(),
+            startAmount: config.bid_price,
+            expirationTime:Math.round(Date.now() / 1000 + 60 * 60 * config.expiration)
+        }).catch(err => {debug(3, 'bidding failed, pushing asset back into array'); assets.push(_asset);});
+    }
+
+    setTimeout(providerThread, config.timeout, _provider);
+}
+
+
+config.targets.forEach(target => {
+    var current_id = 0;
+    while (current_id < target.supply){
+        assets.push({"tokenId":String(current_id),"address":target.address});
+        current_id++;
+    }
+})
+
+
+providers.forEach(provider => {
+    debug(2, `starting provider ${provider.provider.getAddress()}`);
+    providerThread(provider);
+});
